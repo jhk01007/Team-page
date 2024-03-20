fd4d283f49f7192ac9a53f9a1c6d117c3c875226
jhk01007 <75934142+jhk01007@users.noreply.github.com>
2024-03-20 12:59:13 +0900

    third

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -10,11 +10,11 @@ function TeamSection() {
     <div className="team-section" >
       <div className="row">
         <div className="col-md-6">
-          <img src='/images/team.png' alt="Team" className="img-fluid" style={{ marginRight: '30px' }}/>
+          <img src='/images/team.png' alt="Team" className="img-fluid team-all-img" style={{ marginRight: '30px' }}/>
         </div>
         <div className="col-md-6 d-flex flex-column justify-content-center align-items-start text-container">
           <h1>Sweat</h1>
-          <p className="mb-4">"열정과 장인정신으로 땀 흘리다"</p>
+          <p className="mb-4 team-slogun">"열정과 장인정신으로 땀 흘리다"</p>
           <div className="buttons">
             <button className="btn btn-primary mb-2" onClick={goMembers}>See Members</button>
           </div>
